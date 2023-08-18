d2d157e5e54764ac7009d9d4a19a435423e3374a
852us <paul.sunspipe@gmail.com>
2023-08-18 17:11:06 +0800

    Create uip.sh

---
+++ aws/uip.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+W2_IID="i-03560e38a39741de9"
+
+update_ip() {
+  IID=$1
+  echo "Instance-ID: $IID"
+  OldIP=$(aws ec2 describe-instances --instance-id $IID | grep PublicIp | awk -F'[:"]' 'NR==2{print $5}')
+  echo $OldIP
+  
+  # stop instances
+  echo "停止实例..."
+  aws ec2 stop-instances --instance-id $IID
+  
+  echo "等待3分钟..."
+  sleep 180
+  
+  # start instances
+  echo "启动实例..."
+  aws ec2 start-instances --instance-id $IID
+  
+  echo "等待3分钟..."
+  sleep 180
+
+  NewIP=$(aws ec2 describe-instances --instance-id $IID | grep PublicIp | awk -F'[:"]' 'NR==2{print $5}')
+  echo $NewIP
+}
+
+update_ip $W2_IID
+./ddns.sh -n w2.gocoin.one -p $NewIP

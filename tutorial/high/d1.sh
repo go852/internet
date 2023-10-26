7297a8501e355e93ad598ed8a1c9373de628bf91
852us <paul.sunspipe@gmail.com>
2023-10-27 07:31:14 +0800

    Update d1.sh

--- tutorial/high/d1.sh
+++ tutorial/high/d1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # In macOS system, install GNU Bash, and run the command as :
-# bash d3.sh
+# bash d1.sh
 
 declare -A books=(
   ["苏教版数学七年级上册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/6e764703-6e5e-4ea3-9462-34652c2678ef.pkg/thumbnails/1.png" 

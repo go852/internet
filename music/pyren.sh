c761088b44cc0983733046110cf1eabc1f5a7db5
852us <paul.sunspipe@gmail.com>
2023-10-27 17:55:39 +0800

    Update pyren.sh

--- music/pyren.sh
+++ music/pyren.sh
@@ -2,6 +2,7 @@
 shopt -s nullglob
 
 PINYIN="$(pwd)/pinyin.py"
+
 usage() {
   while getopts ":e:s:t:" opt; do
     case $opt in
@@ -10,12 +11,17 @@ usage() {
       ext=$OPTARG
       ;;
     s)
-      echo "source: $OPTARG"
-      source=$OPTARG
+      echo "source_dir: $OPTARG"
+      source_dir=$(cd $OPTARG; pwd)
       ;;
     t)
-      echo "target: $OPTARG"
-      target=$OPTARG
+      echo "target_dir: $OPTARG"
+      target_dir=$OPTARG
+      if [ ! -d "$target_dir" ] ; then
+        echo mkdir -p "$target_dir";
+        mkdir -p "$target_dir";
+      fi
+      target_dir=$(cd $target_dir; pwd)
       ;;
     \?)
       echo "无效参数：-$OPTARG" >&2
@@ -25,8 +31,8 @@ usage() {
   done
   
   shift $((OPTIND -1)) 
-  
-  if [[ (-z "$ext") ||  (-z "$source") || ( -z "$target") ]] ; then
+
+  if [[ (-z "$ext") ||  (-z "$source_dir") || ( -z "$target_dir") ]] ; then
     echo "使用说明："
     echo "  $0 -e 扩展名 -s 源目录 -t 目标目录"
     echo "示例："
@@ -35,38 +41,41 @@ usage() {
   fi
 }
 
+process_dir(){
+  source="$1"
+  target="$2"
 
-process_dir(){  
-  PWD="$(pwd)"
-  if [[ "${source:0:1}" != "/" ]]; then
-    source="$PWD/$source"
+  echo "source: $source"
+  echo "target: $target"
+
+  if [ ! -d "$target" ] ; then
+    echo mkdir -p "$target";
+    mkdir -p "$target";
   fi
-  if [[ "${target:0:1}" != "/" ]]; then
-    target="$PWD/$target"
-  fi
-  
+
   cd $source
+  for f in *.$ext; do
+    f2=${f#*-}
+    newname=$(python3 $PINYIN "$f2")
+    dest_filename="$target/$newname"
+    echo "$f: $dest_filename"
+    cp "$f" "$dest_filename"
+  done
+
   for d in *; do
     if [ -d "$d" ] ; then
-      echo "处理目录：$d"
-      newd=$(python3 $PINYIN "$d")
-      dest="$target/$newd"
-      if [ ! -d "$dest" ] ; then mkdir -p "$dest"; fi
-      cd "$d"
-      for f in *.$ext; do
-        f2=${f#*-}
-        newname=$(python3 $PINYIN "$f2")
-        dest_filename="$dest/$newname"
-        echo "$f: $dest_filename"
-        cp "$f" "$dest_filename"
-      done
-      echo
-      cd -
+      dir_name=$(cd $d; pwd)
+      newd=$(python3 $PINYIN "${dir_name#$source_dir/}")
+      echo "待处理的目录：$newd"
+      dest="$target_dir/$newd"
+      echo "process_dir $d $dest"
+      process_dir $d $dest
     fi
   done
   cd -
+#  echo "完成$source 目录的处理..."
 }
 
 usage $*
-process_dir
+process_dir $source_dir $target_dir
 

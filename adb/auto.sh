aa7aaaaf378d559adb5b9aa9cda5df031335f54f
852us <paul.sunspipe@gmail.com>
2023-12-15 18:48:35 +0800

    Update auto.sh

--- adb/auto.sh
+++ adb/auto.sh
@@ -24,11 +24,13 @@ swipe(){
 }
 
 while true; do
-  t=$(echo "scale=2;$RANDOM % 3999 / 10 + 3.27" | bc)
+  t=$(echo "scale=2;$RANDOM % 1999 / 5 + 1.27" | bc)
+  #sign=$(if [ $((RANDOM % 10)) -eq 1 ] ; then echo "1"; else echo "-1"; fi)
+  sign="-1"
   x1=$((XMAX * (RANDOM % 10 + 45) / 100))
-  y1=$((YMAX * (RANDOM % 10 + 80) / 100))
-  x2=$((x1 + XMAX * (RANDOM % 10) / 100))
-  y2=$((y1 - YMAX * (RANDOM % 30 + 30) / 100))
+  y1=$((YMAX * (RANDOM % 10 + 50) / 100))
+  x2=$((x1 + XMAX * (RANDOM % 8) / 100))
+  y2=$((y1 + sign * YMAX * (RANDOM % 10 + 25) / 100))
   echo "${t}s: $x1 $y1 $x2 $y2"
   swipe $x1 $y1 $x2 $y2
   sleep $t

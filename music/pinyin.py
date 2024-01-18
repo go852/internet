7ca932a59b4346fc791eaf9e6e762d7e1f4148e4
852us <paul.sunspipe@gmail.com>
2024-01-18 18:08:07 +0800

    Update pinyin.py

--- music/pinyin.py
+++ music/pinyin.py
@@ -4,10 +4,10 @@ from xpinyin import Pinyin
 
 def pinyin_capitalize(str):
   p = Pinyin()
-  py = p.get_pinyin(str, '@@')
+  py = p.get_pinyin(str, '@@', tone_marks=1)
   py2 = py.split('@@')
   py3 = [c.capitalize() for c in py2]
-  py4 = ''.join(py3[0:])
+  py4 = ' '.join(py3[0:])
   print(py4)
   return py4
 

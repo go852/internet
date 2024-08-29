db06ea5f69bf532b244d7686821567c0658dd18a
852us <ccjc2030@outlook.com>
2024-08-29 20:47:11 +0800

    Update d1.sh

--- tutorial/middle/d1.sh
+++ tutorial/middle/d1.sh
@@ -4,40 +4,29 @@
 # bash d1.sh
 
 declare -A books=(
-  ["统编版道德与法治一年级上册"]="https://r3-ndr.ykt.cbern.com.cn/edu_product/65/document/a99f662acf204783844599c56f2bfb1d/image/1.jpg"
-  ["苏教版数学七年级上册"]="https://r3-ndr.ykt.cbern.com.cn/edu_product/esp/assets/01e365c7-9178-4155-8640-c862c5eff5e6.t/zh-CN/1688377923908/transcode/image/1.jpg?v=1688380045451"
-  ["苏教版数学七年级下册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/f156dcad-f112-4937-a4bc-f4fa349beed6.t/zh-CN/1688378312803/transcode/image/1.jpg?v=1688392469062"
-  ["苏教版数学八年级上册"]="https://r3-ndr.ykt.cbern.com.cn/edu_product/esp/assets/898de31a-e932-43ea-96af-83df3300020b.t/zh-CN/1688378175799/transcode/image/1.jpg?v=1688386577924"
-  ["苏教版数学八年级下册"]="https://r2-ndr.ykt.cbern.com.cn/edu_product/esp/assets/e2eb2248-6d8e-44fc-a1ef-a1eaead72992.t/zh-CN/1688378294463/transcode/image/1.jpg?v=1688391748081"
-  ["苏教版数学九年级上册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/bd633723-cfca-47c7-bea3-7496e1648a91.t/zh-CN/1688378249776/transcode/image/1.jpg?v=1688389495827"
-  ["苏教版数学九年级下册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/33f1be2a-c063-4669-ba44-341c2b17a091.t/zh-CN/1688378050463/transcode/image/1.jpg?v=1688382757371"
-  ["人教版生物学八年级上册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/5a29010d-b2f0-4bf0-9e05-e311168cd929.pkg/thumbnails/2023.07.04_林鑫.png"
-  ["人教版生物学八年级下册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/f4e7eb1c-2a75-4bc5-8fb9-404a108e1b84.t/zh-CN/1688378317278/transcode/image/1.jpg?v=1688392486061"
-  ["数学（A版）必修第一册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/6e764703-6e5e-4ea3-9462-34652c2678ef.pkg/thumbnails/1.png" 
-  ["数学（A版）必修第二册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/d296fc79-8d47-4b18-862c-6df49adc2ce0.pkg/thumbnails/1.png"
-  ["数学（A版）选修第一册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/d0fd2c1f-6b4f-43f0-8229-de0a53b197df.pkg/thumbnails/1.png"
-  ["数学（A版）选修第二册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/99c1fb5b-d1e0-4238-90b9-a573ab84bf08.pkg/thumbnails/1.png"
-  ["数学（A版）选修第三册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/ffaba6c3-497d-47b0-b91a-784f43625507.pkg/thumbnails/1.png"
-  ["物理必修第一册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/708256b6-6f06-4d14-89c7-4df16dfe3b81.pkg/thumbnails/1.png"
-  ["物理必修第二册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/55baa3cc-156f-4358-8e28-bfa21a864450.pkg/thumbnails/1.png"
-  ["物理必修第三册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/dcd8cc6b-5380-4008-a2d0-a061f24d34dd.pkg/thumbnails/1.png"
-  ["物理选修第一册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/346c3c04-1663-472c-849e-ff876dcf293f.pkg/thumbnails/1.png"
-  ["物理选修第二册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/2ee7d7fa-1920-4d37-a179-91d5fd59b8c1.pkg/thumbnails/1.png"
-  ["物理选修第三册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/2109c25c-2e52-4da3-8ab3-18cbe632ec11.pkg/thumbnails/1.png"
-  ["化学必修第一册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/5cd19072-e40d-4a73-8580-7b7ada5d4005.pkg/thumbnails/1.png"
-  ["化学必修第二册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/07f7d663-a867-4eb6-ad39-03b55dbd4a65.pkg/thumbnails/1.png"
-  ["化学选修第一册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/3502fe81-b23e-4f68-aa3d-7921e7932ec9.pkg/thumbnails/1.png"
-  ["化学选修第二册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/b82cefe7-d631-4bde-baf9-352ca033cba4.pkg/thumbnails/1.png"
-  ["化学选修第三册"]="https://r1-ndr.ykt.cbern.com.cn/edu_product/esp/assets/c561d8ee-7c06-4cb1-9a4d-e34036f02d53.pkg/thumbnails/1.png"
+  ["语文七年级上册"]="https://v1.ykt.cbern.com.cn/65/document/9f996e9b45d847339b4f07d5db80bd43/pdf.pdf"
+  ["语文七年级下册"]="https://v1.ykt.cbern.com.cn/65/document/206240c75072408bb343d0c403696efa/pdf.pdf"
+  ["语文八年级上册"]="https://v3.ykt.cbern.com.cn/65/document/afce7897a3b44ce48a6daa0f03a06f5a/pdf.pdf"
+  ["语文八年级下册"]="https://v3.ykt.cbern.com.cn/65/document/030cc3fde02044ef992260b739792925/pdf.pdf"
+  ["语文九年级上册"]="https://v3.ykt.cbern.com.cn/65/document/69ec38b3e3da416ab984ffd76cdec58e/pdf.pdf"
+  ["语文九年级下册"]="https://v2.ykt.cbern.com.cn/65/document/4558cc2a9eac4098ac012f7778d23163/pdf.pdf"
+  ["数学七年级上册"]="https://v1.ykt.cbern.com.cn/65/document/1f950f67804f41afad067332c90281e0/pdf.pdf"
+  ["数学七年级下册"]="https://v3.ykt.cbern.com.cn/65/document/c48d85ba39b945bcb8d9a6d2ce0ff660/pdf.pdf"
+  ["数学八年级上册"]="https://v2.ykt.cbern.com.cn/65/document/e2ca61551006430db66554049764a8b6/pdf.pdf"
+  ["数学八年级下册"]="https://v2.ykt.cbern.com.cn/65/document/f14ca004aa554c38aa838424f6e145c8/pdf.pdf"
+  ["数学九年级上册"]="https://v1.ykt.cbern.com.cn/65/document/bb58ee260f5f4d97b232fc2bfd6a56e7/pdf.pdf"
+  ["数学九年级下册"]="https://v3.ykt.cbern.com.cn/65/document/38ac2ce34b304b6ea44a95b39231e5d6/pdf.pdf"
+  ["物理八年级上册"]="https://v3.ykt.cbern.com.cn/65/document/455b5519a1b54154b81bca9bcb75d8b0/pdf.pdf"
+  ["物理八年级下册"]="https://v3.ykt.cbern.com.cn/65/document/9db08b6e44854f2ea9450acf9a64effe/pdf.pdf"
+  ["物理九年级上册"]="https://v1.ykt.cbern.com.cn/65/document/6f5d9380d1e0421dbf8ee9ec9312dec9/pdf.pdf"
+  ["物理九年级下册"]="https://v3.ykt.cbern.com.cn/65/document/90f98efd11b94f339133779144ed3011/pdf.pdf"
+  ["化学九年级上册"]="https://v2.ykt.cbern.com.cn/65/document/c257d888966b443481f3db28214df28e/pdf.pdf"
+  ["化学九年级下册"]="https://v3.ykt.cbern.com.cn/65/document/02d52dbeec9746fbae3abfd98cff6e4b/pdf.pdf"
 )
 
 download(){
   for book in ${!books[*]};do
     url=${books[$book]}
-    url=${url/.t*/.pkg}
-    url=${url/thumbnails*/}
-    url=${url/'image/1.jpg'/}
-    url="${url}pdf.pdf?v=1688392469062"
     echo $url
 
     if [ ! -f "${book}.pdf" ]; then
@@ -48,4 +37,5 @@ download(){
 }
 
 download
+
 

438e32ef86495c2a7fdafe3fb1a4cce68caf5da8
852us <ccjc2030@outlook.com>
2024-08-30 10:55:00 +0800

    Update click.py

--- tiktok/macos/click.py
+++ tiktok/macos/click.py
@@ -4,18 +4,35 @@ import time
 import random
 import pyautogui
 
-width, height = pyautogui.size()
-print(width, height)
+def get_current_app():
+  import AppKit
+  NSWorkspace = AppKit.NSWorkspace.sharedWorkspace()
+  active_app_name = NSWorkspace.activeApplication()['NSApplicationName']
+  return active_app_name
 
-x, y = pyautogui.position()
-print("00000 x: %d, y: %d", x,y)
-while True:
-  # 移动鼠标到屏幕上的(x, y)位置
-  x = int(width*0.35) + random.randint(0, int(width/20))
-  y = int(height*0.5) + random.randint(0, int(height/20))
-  #print("x: %d, y: %d", x,y)
-  # 在(x, y)位置单击
-  pyautogui.click(x, y)
-  pyautogui.click()
-  time.sleep(random.uniform(0.3, 0.4))
+def autoclick(n):
+  total_number = 0
+  total_seconds = 0
+  width, height = pyautogui.size()
+  print("屏幕像素: ", width, height)
 
+  while True:
+    if get_current_app() != "抖音":
+      continue
+
+    x = int(width*0.35) + random.randint(0, int(width/100))
+    y = int(height*0.5) + random.randint(0, int(height/100))
+    print("x: %d, y: %d" %(x,y))
+  
+    pyautogui.click(x, y)
+    pyautogui.click()
+    total_number += 1
+    print("点赞用时：%.1f秒,  次数：%d" % (total_seconds, total_number))
+    if total_number % 1000 == 0:
+      time.sleep(300)
+    else:
+      seconds = random.uniform(0.1, 0.2)
+      total_seconds += seconds
+      time.sleep(seconds)
+
+autoclick(50000)
